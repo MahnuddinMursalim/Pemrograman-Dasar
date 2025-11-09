b15255d7b62fc99841b2e5012922667a214f2d95
MahnuddinMursalim <mahmuddinmursalim5@gmail.com>
2025-11-09 21:24:57 +0800

    Add files via upload

---
+++ LUAS PERSEGI.py
@@ -0,0 +1,10 @@
+# Nama : Mahmuddin Mursalim
+# nim : d0425509
+# luas PERSEGI
+
+sisi = int(input("Masukkan sisi pertama: "))
+sisikedua = int(input("Masukkan sisi kedua: "))
+
+luas = int(sisi * sisikedua)
+
+print('luas persegi adalah: ' , luas)
