bed587be96ee65f3a4f3ed1479926ae09b5927d1
MahnuddinMursalim <mahmuddinmursalim5@gmail.com>
2025-11-09 21:24:27 +0800

    Add files via upload

---
+++ LUAS SEGITIGA.py
@@ -0,0 +1,10 @@
+# Nama : Mahmuddin Mursalim
+# nim : d0425509
+# luas SEGITIGA
+
+alas = int(input("Masukkan alas segitiga: "))
+tinggi = int(input("Masukkan tinggi segitiga: "))
+
+luas = int((alas * tinggi) / 2)
+
+print('luas segitiga adalah: ' , luas)
