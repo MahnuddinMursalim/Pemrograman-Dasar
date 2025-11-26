7c9ba7ffaea62b5fe0f6a91c24c49290063907f6
Mahmuddin Mursalim <mahmuddinmursalim5@gmail.com>
2025-11-27 07:39:27 +0800

    Add files via upload

---
+++ ARRAY.py
@@ -0,0 +1,29 @@
+# NAMA		: MAHMUDDIN MURSALIM
+# NIM			: D0425529
+# KELAS		: B SISTEM INFORMASI
+# PROGRAM ARRAY
+
+mahasiswa = ["ibrahim", "kurniawan", "jona irwansyah", "nopal"]
+
+# Menampilkan semua nama
+print("Daftar Mahasiswa : ", mahasiswa)
+
+# Mengakses 
+print("Mahasiswa pertama : ", mahasiswa[0])
+
+#Mengakses
+print("Mahasiswa kedua : ", mahasiswa[1])
+
+#Mengakses
+print("Mahasiswa ketiga : ", mahasiswa[2])
+
+#Mengakses
+print("Mahasiswa keempat : ", mahasiswa[3])
+
+# Menambah data 
+mahasiswa.append("muh alim wisjaya")
+print("Setelah ditambah : ", mahasiswa)
+
+# Menghapus 
+mahasiswa.remove("jona irwansyah")
+print("Setelah jona irwansyah dihapus : ", mahasiswa)
